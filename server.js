093fd6d7e57180683cee54c1a3816a8e0b281e83
Ubuntu <ubuntu@ip-172-31-33-59.us-west-2.compute.internal>
2018-10-20 17:33:36 +0000

    no bots

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var compression = require('compression');
 var morgan = require('morgan');
+var noBots = require('express-nobots');
 var app = express();
 app.use(morgan('":remote-addr",":date[web]",":method",":url",":status",":response-time ms"'));
 app.use(compression());
+app.use(noBots());
 
 console.log('"ip","date","method","url","status","time"');
 
